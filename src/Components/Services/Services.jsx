926d064d7fef7ac2a95d5cce6c95524452338699
MuhammadRizwan-29 <engrrizwanjamali@gmail.com>
2025-06-02 19:44:43 +0500

    Services section has been added

---
+++ src/Components/Services/Services.jsx
@@ -0,0 +1,27 @@
+import "./Services.css";
+import Services_Data from "../../assets/services_data";
+import arrow_icon from "../../assets/arrow_icon.svg";
+import SectionTitle from "../SectionTitle/SectionTitle";
+
+export default function Services() {
+  return (
+    <section className="container services">
+      <SectionTitle title={"My Services"} />
+      <div className="cards">
+        {Services_Data.map((service, index) => {
+          return (
+            <div className="card" key={index}>
+              <h3>{service.s_no}</h3>
+              <h2>{service.s_name}</h2>
+              <p>{service.s_desc}</p>
+              <div className="readmore">
+                <p>Read More</p>
+                <img src={arrow_icon} alt="Arrow Icon" />
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+}
