1258d26e571c12bc10c034324b8c70c9867fb05c
MuhammadRizwan-29 <engrrizwanjamali@gmail.com>
2025-06-03 15:36:37 +0500

    Contact Section has been updated. Also updated the App.js file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import About from "./Components/About/About";
+import Contact from "./Components/Contact/Contact";
+import Footer from "./Components/Footer/Footer";
 import Hero from "./Components/Hero/Hero";
+import MyWork from "./Components/MyWork/MyWork";
 import Navbar from "./Components/Navbar/Navbar";
+import Services from "./Components/Services/Services";
 
 function App() {
   return (
@@ -9,6 +13,9 @@ function App() {
       <main>
         <Hero />
         <About />
+        <Services />
+        <MyWork />
+        <Contact />
       </main>
     </>
   );
---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,52 @@
+import SectionTitle from "../SectionTitle/SectionTitle";
+import "./Contact.css";
+import mail_icon from "../../assets/mail_icon.svg";
+import location_icon from "../../assets/location_icon.svg";
+import call from "../../assets/call_icon.svg";
+
+export default function Contact() {
+  return (
+    <section className="container contact">
+      <SectionTitle title={"Get in touch"} />
+      <div className="details">
+        <div className="left">
+          <h1>Let's talk</h1>
+          <p>
+            I'm currently avaliable to take on new projects, so feel free to
+            send me a message about anything that you want me to work on. You
+            can contact anytime.
+          </p>
+          <div className="contact_details">
+            <div className="detail">
+              <img src={mail_icon} alt="Contact Detail Icon" />
+              <p>mrdev@portfolio.com</p>
+            </div>
+            <div className="detail">
+              <img src={call} alt="Contact Detail Icon" />
+              <p>+92 300 0000000</p>
+            </div>
+            <div className="detail">
+              <img src={location_icon} alt="Contact Detail Icon" />
+              <p>Main Bizerta Line, Sharah-e-Faisal Karachi</p>
+            </div>
+          </div>
+        </div>
+        <div className="right">
+          <form>
+            <label>Your Name</label>
+            <input type="text" name="name" placeholder="Enter your name" />
+            <label>Your Email</label>
+            <input type="email" name="email" placeholder="Enter your Email" />
+            <label>Write your message</label>
+            <textarea
+              name="message"
+              rows="8"
+              placeholder="Enter your Message"
+            ></textarea>
+            <button type="submit">Submit Now</button>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+}
