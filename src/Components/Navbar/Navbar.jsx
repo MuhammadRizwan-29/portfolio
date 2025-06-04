34974a2da874f5aa433584527dcf66c8ac8cbe90
MuhammadRizwan-29 <engrrizwanjamali@gmail.com>
2025-06-04 10:44:51 +0500

    Mobile menu work has been done. Also also companents made responsive to the mobile-device.

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,15 +1,34 @@
 import "./Navbar.css";
 import logo from "../../assets/logo.svg";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import underline from "../../assets/nav_underline.svg";
 import AnchorLink from "react-anchor-link-smooth-scroll";
+import menu_open from "../../assets/menu_open.svg";
+import menu_close from "../../assets/menu_close.svg";
 
 export default function Navbar() {
   const [menu, setMenu] = useState("home");
+  const menuRef = useRef();
+
+  const openMenu = () => {
+    menuRef.current.style.right = "0";
+  };
+
+  const closeMenu = () => {
+    menuRef.current.style.right = "-350px";
+  };
+
   return (
     <header className="container">
       <img src={logo} alt="Logo" />
-      <ul>
+      <img src={menu_open} onClick={openMenu} alt="menu icon" />
+      <ul ref={menuRef}>
+        <img
+          src={menu_close}
+          onClick={closeMenu}
+          alt="close menu"
+          className="close-icon"
+        />
         <li>
           <AnchorLink className="anchor_link" href="#home">
             <p onClick={() => setMenu("home")}>Home</p>
